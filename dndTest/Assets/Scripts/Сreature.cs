f9cbe68f59d4a342a218eaecc24faf436db0aa33
Ukropchik <vova.chakalov.2003@gmail.com>
2022-10-02 22:25:07 +0300

    Дописал класс существо на минималках

--- dndTest/Assets/Scripts/Сreature.cs
+++ dndTest/Assets/Scripts/Сreature.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,11 +10,27 @@ namespace Pumba
         public string Name;
         public string Description;
 
-        public  Creature(string name, string description)
-        {
-            Name = name;
-            Description = description;
-        }
+        public int ProtectionClass;
+        public int Hits;
+        public string HitsFormula;
+        public int Speed;
+
+        //Dictionary<string,int> Characteristics;
+
+        public int Strength;
+        public int Dexterity;
+        public int BodyType;
+        public int Intelligence;
+        public int Wisdom;
+        public int Charisma;
+
+        public string[] Skills;
+        public string Senses;
+        public string[] Languages;
+        public string Danger;
+
+        public string[] Actions;
+
     }
 
 }
